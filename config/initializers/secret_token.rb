11f05b4b2a96a44140b26bc37a13e892dbacb906
Rebecca Hunter <hrmrebecca@gmail.com>
2013-01-03 13:43:05 -0500

    initial commit of the app
    I think I (finally) have the hang of this.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Familytracker::Application.config.secret_token = '91b3ac3c4b2cb5664ec45bb3fa9e14ab37547989b4ca4ed1f38ef30cad16b33455bb81f9cbebc3b7f30202a41fe2d0d8f6e795b73ae631703e6af072c92a440a'
